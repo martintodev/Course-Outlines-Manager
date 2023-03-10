14b8cfcc3456509c6b9165df70216a0f78d93e7a
Martin Todev <mtodev@uwo.ca>
2023-03-10 15:54:22 -0500

    Fixed error

--- backend-express/Authentication/authController.js
+++ backend-express/Authentication/authController.js
@@ -63,7 +63,7 @@ module.exports.login = async (req, res) => {
         const token = createToken(user._id);
 
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(200).json({ user: user._id })
+        res.status(200).json({ user: user })
     } catch (err) {
         const errors = handleErrors(err);
         res.status(400).json({ errors });
